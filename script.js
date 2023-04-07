390939181312a1acd3be78b6ac645fff3965e9b3
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-07 12:42:55 -0500

    Add hp and likesPC attributes to meatbag class. Add describe affinity method.

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ class Meatbag {
         this.name = name;
         this.race = race;
         this.gender = gender;
+        this.hp = 2;
+        this.likesPc = false;
     }
     introduceSelf() {
-        console.log(`My name is ${this.name}. I am a ${this.race} ${this.gender}`);
+        console.log(`My name is ${this.name}. I am a ${this.race} ${this.gender}. I have ${this.hp} health points.`);
+    }
+
+    descAffinity() {
+        if(!this.likePC){
+            console.log('The state of man\'s life is nasty, brutish, and short. I do not trust anyone.')
+        }
+        else {
+            console.log('I have never met a man I did not like!')
+        }
     }
 }
 john = new Meatbag('John', 'Elven', 'Male')
 john.introduceSelf()
+john.descAffinity()
