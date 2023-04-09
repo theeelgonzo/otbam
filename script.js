fb6006a4b90d651447f0388abad977695bbe3876
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-09 18:24:01 -0500

    Add fight method to meatbag class.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('Welcome to the electro jungle.')
+console.log('Welcome to the electro jungle.');
 class Meatbag {
     constructor(name, race, gender) {
         this.name = name;
@@ -6,20 +6,33 @@ class Meatbag {
         this.gender = gender;
         this.hp = 2;
         this.likesPc = false;
+        this.isMeatbag = true;
     }
     introduceSelf() {
-        console.log(`My name is ${this.name}. I am a ${this.race} ${this.gender}. I have ${this.hp} health points.`);
+        console.log(
+            `My name is ${this.name}. I am a ${this.race} ${this.gender}. I have ${this.hp} health points.`
+        );
     }
 
     descAffinity() {
-        if(!this.likePC){
-            console.log('The state of man\'s life is nasty, brutish, and short. I do not trust anyone.')
-        }
-        else {
-            console.log('I have never met a man I did not like!')
+        if (!this.likePC) {
+            console.log(
+                "The state of man's life is nasty, brutish, and short. I do not trust anyone."
+            );
+        } else {
+            console.log('I have never met a man I did not like!');
         }
     }
+    fight(target) {
+        console.log(`${this.name} attacks ${target.name}`);
+        console.log(target);
+    }
 }
-john = new Meatbag('John', 'Elven', 'Male')
-john.introduceSelf()
-john.descAffinity()
+
+john = new Meatbag('John', 'Elven', 'Male');
+john.introduceSelf();
+john.descAffinity();
+
+jordan = new Meatbag('Jordan', 'Human', 'Male');
+jordan.introduceSelf;
+john.fight(jordan);
