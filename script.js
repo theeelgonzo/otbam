b4e02df6de41bb45a0d217afa18026212507fdb3
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-18 10:05:21 -0500

    Initialize Phil.

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+class Meatbag {
+    constructor(name, race, gender) {
+        this.name = name;
+        this.race = race;
+        this.gender = gender;
+        this.hp = 2;
+        this.likesPc = false;
+        this.isMeatbag = true;
+    }
+    introduceSelf() {
+        console.log(
+            `My name is ${this.name}. I am a ${this.race} ${this.gender}. I have ${this.hp} health points.`
+        );
+    }
+
+    descAffinity() {
+        if (!this.likePC) {
+            console.log(
+                "The state of man's life is nasty, brutish, and short. I do not trust anyone."
+            );
+        } else {
+            console.log('I have never met a man I did not like!');
+        }
+    }
+    fight(target) {
+        console.log(`${this.name} attacks ${target.name}`);
+        console.log(target);
+    }
+}
+
+class PlayerCharacter extends Meatbag {
+    constructor(name, race, gender, pcLocation, hasKey) {
+        super(name, race, gender);
+        {
+            this.pcLocation = dungeon.roomOne;
+            this.hasKey = false;
+        }
+    } //{
+    //super(name, race, gender);
+    //}
+
+    talk(target, quote) {
+        if (typeof target == 'undefined') {
+            console.log(typeof target);
+            console.log(`You cannot speak to ${target} right now.`);
+        } else {
+            console.log(typeof target);
+            console.log(`${target.name} would love to talk to you.`);
+        }
+    }
+    walk() {
+        console.log('There you go, walking again.');
+    }
+    look() {
+        const tar = prompt('What are you looking at?');
+        console.log(`That is a rather nice ${tar}.`);
+    }
+}
 //A door class. Use for generating doors in rooms & halls.
 //hp for breakability, not yet in use. Use small integer value. Name for clarity, ie. 'North Door' or 'South Door'. Description populates describe method for PC examine method. isLocked is a boolean value. Does exactly what it says on the tin.
 class Door {
@@ -9,7 +67,7 @@ class Door {
         this.destination = destination;
     }
     goThru() {
-        if (!isLocked) {
+        if (!this.isLocked) {
             boxPrint(`You went through the ${this.name}`);
             dungeon.pcLocation = this.destination;
             return dungeon.pcLocation;
@@ -100,7 +158,7 @@ const dungeon = {
             'a typical wooden door...not much to look at',
             roomTwo
         ),
-        monster: new Meatbag('Phillip', 'orc', 'male'),
+        monster: (phil = new Meatbag('Phillip', 'orc', 'male')),
         chest: new Chest(
             'a well-worn old chest',
             'it might have some dustbunnies in it',
@@ -194,64 +252,6 @@ function boxPrint(message) {
 }
 
 console.log('Welcome to the electro jungle.');
-class Meatbag {
-    constructor(name, race, gender) {
-        this.name = name;
-        this.race = race;
-        this.gender = gender;
-        this.hp = 2;
-        this.likesPc = false;
-        this.isMeatbag = true;
-    }
-    introduceSelf() {
-        console.log(
-            `My name is ${this.name}. I am a ${this.race} ${this.gender}. I have ${this.hp} health points.`
-        );
-    }
-
-    descAffinity() {
-        if (!this.likePC) {
-            console.log(
-                "The state of man's life is nasty, brutish, and short. I do not trust anyone."
-            );
-        } else {
-            console.log('I have never met a man I did not like!');
-        }
-    }
-    fight(target) {
-        console.log(`${this.name} attacks ${target.name}`);
-        console.log(target);
-    }
-}
-
-class PlayerCharacter extends Meatbag {
-    constructor(name, race, gender, pcLocation, hasKey) {
-        super(name, race, gender);
-        {
-            this.pcLocation = dungeon.roomOne;
-            this.hasKey = false;
-        }
-    } //{
-    //super(name, race, gender);
-    //}
-
-    talk(target, quote) {
-        if (typeof target == 'undefined') {
-            console.log(typeof target);
-            console.log(`You cannot speak to ${target} right now.`);
-        } else {
-            console.log(typeof target);
-            console.log(`${target.name} would love to talk to you.`);
-        }
-    }
-    walk() {
-        console.log('There you go, walking again.');
-    }
-    look() {
-        const tar = prompt('What are you looking at?');
-        console.log(`That is a rather nice ${tar}.`);
-    }
-}
 
 john = new Meatbag('John', 'Elven', 'Male');
 john.introduceSelf();
